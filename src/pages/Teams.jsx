a0aed74fdefeadfdecc157713b50059889036560
KauraniDivya <kauranidivya@gmail.com>
2024-05-12 20:53:42 +0530

    teams

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import csk from "../components/images/csk.png"
 import rcb from "../components/images/rcb.png"
+import dc from "../components/images/dc.png"
+import gt from "../components/images/gt.png"
+import kkr from "../components/images/kkr.png"
+import lsg from "../components/images/lsg.png"
+import mi from "../components/images/mi.png"
+import pk from "../components/images/pk.png"
+import rr from "../components/images/rr.png"
+import srh from "../components/images/srh.png"
 
 const Teams = () => {
   return (
     <>
-    <div className='container m-4 ml-10 flex gap-8'>
+    <div className='container m-6 flex flex-wrap gap-6 justify-center items-center'>
     <div className="card csk">
     <div className="content">
       <div className="back">
         <div className="back-content">
-<img src={csk} alt='csk logo' className='w-[70%]'/>
-          <strong className='text-xl text-center'>Chennai Super Kings</strong>
+<img src={csk} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Chennai Super <br /> Kings</strong>
         </div>
       </div>
       <div className="front">
@@ -46,7 +54,7 @@ const Teams = () => {
     <div className="content">
       <div className="back">
         <div className="back-content">
-<img src={rcb} alt='csk logo' className='h-[40%]'/>
+<img src={rcb} alt='csk logo' className='h-[60%]'/>
           <strong className='text-xl text-center'>Royal Challengers Bengaluru</strong>
         </div>
       </div>
@@ -67,6 +75,294 @@ const Teams = () => {
             <div className="title">
               <p className="title">
                 <strong>Royal Challengers Bengaluru</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card dc">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={dc} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Delhi Capitals</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">DC</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Delhi Capitals</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card gt">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={gt} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Gujarat Titans</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">GT</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Gujarat Titans</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card kkr">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={kkr} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Kolkata Knight <br /> Riders</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">KKR</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Kolkata Knight Riders</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card lsg">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={lsg} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center text-blue-900'>Lucknow Super <br /> Giants</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">LSG</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Lucknow Super Giants</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card mi">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={mi} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Mumbai Indians</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">MI</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Mumbai Indians</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card pk">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={pk} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Punjab Kings</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">PK</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Punjab Kings</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card rr">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={rr} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Rajasthan Royals</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">RR</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Rajasthan Royals</strong>
+              </p>
+              {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
+            </div>
+            <p className="card-footer text-md">
+              2022 &nbsp; | &nbsp; 2021
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="card srh">
+    <div className="content">
+      <div className="back">
+        <div className="back-content">
+<img src={srh} alt='csk logo' className='h-[60%]'/>
+          <strong className='text-xl text-center'>Sunrisers <br /> Hyderabad</strong>
+        </div>
+      </div>
+      <div className="front">
+        
+        <div className="img">
+          <div className="circle">
+          </div>
+          <div className="circle" id="right">
+          </div>
+          <div className="circle" id="bottom">
+          </div>
+        </div>
+  
+        <div className="front-content">
+          <small className="badge">SRH</small>
+          <div className="description">
+            <div className="title">
+              <p className="title">
+                <strong>Sunrisers Hyderabad</strong>
               </p>
               {/* <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg> */}
             </div>
